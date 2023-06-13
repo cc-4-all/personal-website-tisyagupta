4c8ba14e55e9beabdcd6b5da7ab2fa8fde073be9
tisyagupta <136474759+tisyagupta@users.noreply.github.com>
2023-06-13 15:29:18 +0100

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-let pic = document.getElementById("profile-picture");
-
-document.addEventListener("scroll", function () {
-  if (window.scrollY > 5) {
-    pic.style.borderRadius = "0%";
-  } else {
-    pic.style.borderRadius = "100%";
-  }
-});
